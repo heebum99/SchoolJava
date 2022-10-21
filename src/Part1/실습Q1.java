5dc87cc8ad7bb3e6d3318fbe5d0a5b02d18f01de
heebum99 <heebum9955@naver.com>
2022-10-21 22:56:38 +0900

    1장 - 실습문제 1번

---
+++ src/Part1/실습Q1.java
@@ -0,0 +1,8 @@
+package Part1;
+
+public class 실습Q1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome!!");
+        System.out.println("자바 세계로 오신 것을 환영합니다.");
+    }
+}
