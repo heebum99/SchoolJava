3a5d76e928a4abaa803b16d13b15e6ac3201a816
heebum99 <heebum9955@naver.com>
2022-10-24 20:07:18 +0900

    2장 - 예제 2번문제

---
+++ src/Part2/예제Q2_2.java
@@ -0,0 +1,15 @@
+package Part2;
+
+public class 예제Q2_2 {
+    public static void main(String[] args) {
+        System.out.println("2018314009 김희범");
+        final double PI = 3.14;
+
+        double radius = 5.0;
+        double circleArea = radius * radius * PI;
+        double circumference = radius * 2 * PI;
+
+        System.out.println("원의 면적 = "+circleArea);
+        System.out.println("원의 둘레 = "+circumference);
+    }
+}
