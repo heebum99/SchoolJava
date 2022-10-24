fb1178029c8be28f6efad3d2f7e9ec7d44c7c30f
heebum99 <heebum9955@naver.com>
2022-10-24 20:10:18 +0900

    2장 - 예제 3번문제

---
+++ src/Part2/예제Q2_3.java
@@ -0,0 +1,18 @@
+package Part2;
+
+public class 예제Q2_3 {
+    public static void main(String[] args) {
+        System.out.println("2018314009 김희범");
+        byte b = -127;
+        int i = 7;
+
+        System.out.println(b - i);
+        System.out.println(10 / 4);
+        System.out.println(10.0 / 4);
+        System.out.println((char) 0x12340041); //문자 A의 코드
+        System.out.println((byte) (b - i));
+        System.out.println((int) 2.9 + 1.8);
+        System.out.println((int) (2.9 + 1.8));
+        System.out.println((int) 2.9 + (int) 1.8);
+    }
+}
