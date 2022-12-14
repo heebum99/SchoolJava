622e761241b3385671d2d282d8c522b278c4271e
heebum99 <heebum9955@naver.com>
2022-12-14 15:55:14 +0900

    11장 - 예제13번 Action 이벤트를 이용한 콤보박스

---
+++ src/Part11/ComboActionEx.java
@@ -0,0 +1,44 @@
+package Part11;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ComboActionEx extends JFrame {
+    private String[] fruits = {"apple", "banana", "kiwi", "mango"};
+    private ImageIcon[] images = {
+            new ImageIcon("images/apple.jpg"),
+            new ImageIcon("images/banana.jpg"),
+            new ImageIcon("images/kiwi.jpg"),
+            new ImageIcon("images/mango.jpg")
+    };
+    private JLabel imgLabel = new JLabel(images[0]); //이미지 레이블, 초기값을 image 배열 0번째 이미지로
+    private JComboBox<String> strCombo = new JComboBox<>(fruits);
+
+    public ComboActionEx() {
+        setTitle("콤보박스 활용 예제");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Container c = getContentPane();
+        c.setLayout(new FlowLayout());
+        c.add(strCombo);
+        c.add(imgLabel);
+
+        //Action 리스너 등록
+        strCombo.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JComboBox<String> cb = (JComboBox<String>) e.getSource(); //Action 이벤트가 발생한 콤보박스 알아내기
+                int index = cb.getSelectedIndex(); //콤보박스의 선택된 아이템의 인덱스 번호 알아내기
+                imgLabel.setIcon(images[index]); //인덱스의 이미지를 이미지 레이블에 출력
+            }
+        });
+
+        setSize(300, 250);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new ComboActionEx();
+    }
+}
