a0f1a7bdad1c0c44e87da85b967277fb6c35bb0b
heebum99 <heebum9955@naver.com>
2022-12-14 10:41:18 +0900

    11장 - 예제2번 JLabel을 이용한 레이블 만들기

---
+++ src/Part11/LabelEx.java
@@ -0,0 +1,32 @@
+package Part11;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class LabelEx extends JFrame {
+    public LabelEx() {
+        setTitle("레이블 예제");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Container c = getContentPane();
+        c.setLayout(new FlowLayout());
+
+        JLabel textLabel = new JLabel("사랑합니다.");
+
+        ImageIcon beauty = new ImageIcon("images/beauty.jpg"); //이미지 로딩
+        JLabel imageLabel = new JLabel(beauty); //이미지 레이블 생성
+
+        ImageIcon normalIcon = new ImageIcon("images/normalIcon.gif");
+        JLabel label = new JLabel("보고싶으면 전화하세요", normalIcon, SwingConstants.CENTER);
+
+        c.add(textLabel);
+        c.add(imageLabel);
+        c.add(label);
+
+        setSize(400, 600);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new LabelEx();
+    }
+}
